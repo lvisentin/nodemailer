fd6c7fbf8797dce170a4bf1394a67f64f7e2b7b6
lvisentin <lucas@ancient.gg>
2022-04-04 22:31:28 -0300

    upload file and send via email

--- index.ts
+++ index.ts
@@ -1,18 +1,37 @@
 import express from "express";
 import MailController from "./mailer";
 import bodyParser from "body-parser";
+import cors from "cors";
+import multer from "multer";
 
-const jsonParser = bodyParser.json();
 const app = express();
 const port = 3000;
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log("its working");
 });
 
-app.post("/job", jsonParser, (req, res) => {
-  const { name, email, cv, emailMessage } = req.body;
-  MailController.sendEmail(name, email, cv, emailMessage).then(
+app.post("/job", upload.single("cv"), (req, res) => {
+  const { name, email, emailMessage } = req.body;
+  MailController.sendEmail(
+    name,
+    email,
+    req.file?.path || "",
+    emailMessage
+  ).then(
     () =>
       res.status(200).send({
         message: "Email sent",
--- mailer.ts
+++ mailer.ts
@@ -1,31 +1,42 @@
-import 'dotenv/config';
-const nodemailer = require('nodemailer');
+import "dotenv/config";
+const nodemailer = require("nodemailer");
 
 class MailController {
   private transport;
 
-  constructor() { 
+  constructor() {
     this.transport = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
       port: process.env.MAIL_PORT,
       auth: {
         user: process.env.MAILTRAP_USER,
-        pass: process.env.MAILTRAP_PASS
-      }
+        pass: process.env.MAILTRAP_PASS,
+      },
     });
-    
   }
 
-  public sendEmail(name: string, email: string, cv: string, emailMessage: string) {
-    const message = {
+  public sendEmail(
+    name: string,
+    email: string,
+    filePath: string,
+    emailMessage: string
+  ) {
+    const fileName = filePath.split("uploads/");
+    let message = {
       from: email,
-      to: process.env.MAILTRAP_TO,
+      to: process.env.MAIL_TO,
       subject: `Job Application - ${name}`,
-      text: emailMessage
-    }
-    
+      text: `This is an automatically generated email, containing the resume for ${name} - ${email}`,
+      attachments: [
+        {
+          filename: fileName[1],
+          path: `./${filePath}`,
+        },
+      ],
+    };
+
     return this.transport.sendMail(message);
-  }  
+  }
 }
 
 export default new MailController();
